e148ac91d6c10855401fc003caade7aca2075422
Arjun Shatkin <arjunshatkin@Arjuns-MacBook-Pro.local>
2025-10-16 19:34:39 -0400

    error and variable changes

--- src/app/Labs/Lab3/FilterFunction.tsx
+++ src/app/Labs/Lab3/FilterFunction.tsx
@@ -1,16 +1,16 @@
 export default function FilterFunction() {
-    let numberArray1 = [1, 2, 4, 5, 6];
-    const numbersGreaterThan2 = numberArray1.filter((a) => a > 2);
-    const evenNumbers = numberArray1.filter((a) => a % 2 === 0);
-    const oddNumbers = numberArray1.filter((a) => a % 2 !== 0);
-  
-    return (
-      <div id="wd-filter-function">
-        <h4>Filter Function</h4>
-        numbersGreaterThan2 = {numbersGreaterThan2} <br />
-        evenNumbers = {evenNumbers} <br />
-        oddNumbers = {oddNumbers} <hr />
-      </div>
-    );
-  }
-  
+  const numberArray1 = [1, 2, 4, 5, 6];
+  const numbersGreaterThan2 = numberArray1.filter((a) => a > 2);
+  const evenNumbers = numberArray1.filter((a) => a % 2 === 0);
+  const oddNumbers = numberArray1.filter((a) => a % 2 !== 0);
+
+  return (
+    <div id="wd-filter-function">
+      <h4>Filter Function</h4>
+      <p>numbersGreaterThan2: {numbersGreaterThan2.join(", ")}</p>
+      <p>evenNumbers: {evenNumbers.join(", ")}</p>
+      <p>oddNumbers: {oddNumbers.join(", ")}</p>
+      <hr />
+    </div>
+  );
+}
