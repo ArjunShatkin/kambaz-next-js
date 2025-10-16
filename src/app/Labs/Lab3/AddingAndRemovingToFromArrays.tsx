f9ddf09e81c61af2c5ecbc0e19443cf8b1f0194a
Arjun Shatkin <arjunshatkin@Arjuns-MacBook-Pro.local>
2025-10-16 19:24:36 -0400

    test errors

--- src/app/Labs/Lab3/AddingAndRemovingToFromArrays.tsx
+++ src/app/Labs/Lab3/AddingAndRemovingToFromArrays.tsx
@@ -1,27 +1,29 @@
 export default function AddingAndRemovingToFromArrays() {
-    let numberArray1 = [1, 2, 3, 4, 5];
-    let stringArray1 = ["string1", "string2"];
-    let todoArray = [
-      <li key="1">Buy milk</li>,
-      <li key="2">Feed the pets</li>
-    ];
-  
-    numberArray1.push(6); // adding new items
-    stringArray1.push("string3");
-    todoArray.push(<li key="3">Walk the dogs</li>);
-  
-    numberArray1.splice(2, 1); // remove 1 item starting at 2
-    stringArray1.splice(1, 1);
-  
-    return (
-      <div id="wd-adding-removing-from-arrays">
-        <h4>Add/remove to/from arrays</h4>
-        numberArray1 = {numberArray1} <br />
-        stringArray1 = {stringArray1} <br />
-        Todo list:
-        <ol>{todoArray}</ol>
-        <hr />
-      </div>
-    );
-  }
+  const numberArray1 = [1, 2, 3, 4, 5];
+  const stringArray1 = ["string1", "string2"];
+  const todoArray = [
+    <li key="1">Buy milk</li>,
+    <li key="2">Feed the pets</li>
+  ];
+
+  // Mutate arrays
+  numberArray1.push(6); // add new items
+  stringArray1.push("string3");
+  todoArray.push(<li key="3">Walk the dogs</li>);
+
+  numberArray1.splice(2, 1); // remove 1 item starting at index 2
+  stringArray1.splice(1, 1);
+
+  return (
+    <div id="wd-adding-removing-from-arrays">
+      <h4>Add/remove to/from arrays</h4>
+      <p>numberArray1: {numberArray1.join(", ")}</p>
+      <p>stringArray1: {stringArray1.join(", ")}</p>
+      <p>Todo list:</p>
+      <ol>{todoArray}</ol>
+      <hr />
+    </div>
+  );
+}
+
   
