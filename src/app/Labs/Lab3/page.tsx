e2f67582304637291b713a4bc6475f412e38e535
Arjun Shatkin <arjunshatkin@Arjuns-MacBook-Pro.local>
2025-10-16 19:21:15 -0400

    more error changes

--- src/app/Labs/Lab3/page.tsx
+++ src/app/Labs/Lab3/page.tsx
@@ -30,7 +30,6 @@ import VariableTypes from "./VariableTypes";
 import Add from "./Add";
 import Square from "./Square";
 import Highlight from "./Highlight";
-import AddPathParameters from "./AddPathParameters";
 import Link from "next/link";
 
 export default function Lab3() {
